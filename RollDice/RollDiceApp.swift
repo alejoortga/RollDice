29e295fd676352a764daaefbdacfb2790e1b18e2
Cristian Alejandro Forero <cristian-forero@upc.edu.co>
2023-11-27 18:45:06 -0500

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Cristian Alejandro Ortega on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
