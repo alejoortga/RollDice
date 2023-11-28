3cf076f8df02e71716c92b0de7a8ab50c19bc15a
Cristian Alejandro Forero <cristian-forero@upc.edu.co>
2023-11-27 21:38:17 -0500

    Neon dice colors

--- RollDice/ContentView.swift
+++ RollDice/ContentView.swift
@@ -16,32 +16,42 @@ struct ContentView: View {
     @State private var rightEmpty  = 0
     
     @State private var leftValue = 1
-    @State private var rightValue = 2 
+    @State private var rightValue = 2
     
     var body: some View {
         
         ZStack{
             
+            Color.black.ignoresSafeArea(.all)
+            
             
             VStack{
                 Text("Random Dice")
+                    .font(.system(size: 40, design: .rounded).bold())
+                    .foregroundStyle(.white)
+
                 Spacer()
                 
                 VStack{
                     HStack{
                         Circle().frame(width: 150).overlay{
                             Text("\(rightValue)")
+                                
                                 .foregroundStyle(.white)
                             
                         }
+                        .shadow(color: .white, radius: 10, x:5, y:-5)
                         Circle().frame(width: 150).overlay{
                             Text("\(leftValue)")
                                 .foregroundStyle(.white)
+                             
+                                
                             
-                        }
+                        }.shadow(color: .white, radius: 10, x:5, y:-5)
                     }.padding(.horizontal,30)
                         .padding(.bottom,40)
-                        .font(.system(size: 30,design: .rounded).bold())
+                        .font(.system(size: 100,design: .rounded).bold())
+                        .foregroundColor(.clear)
         
                     Button(action: {
                         let randomElementleft = Int.random(in: 0...4)
@@ -56,10 +66,6 @@ struct ContentView: View {
                              rightValue = rightEmpty
                             
                         }
-                        
-                        
-
-                        
                         if leftEmpty == rightEmpty{
                             print("They are the same")
                             
@@ -72,7 +78,17 @@ struct ContentView: View {
                         
                         
                     }, label: {
-                        Text("RollDice")
+                        VStack{
+                            Text("ROLL")
+                                .foregroundStyle(.white)
+                                .font(.system(size: 30, design: .rounded).bold())
+                        }.padding()
+                        .background(.clear)
+                        .cornerRadius(20) /// make the background rounded
+                        .overlay( /// apply a rounded border
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke(.purple, lineWidth: 2).shadow(color: .white, radius: 5, x: 1, y: 1)
+                        )
                     })
                 }
             }
