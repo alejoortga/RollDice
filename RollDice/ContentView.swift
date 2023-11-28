bdc811e9981c98997075ede5f8ac8e59f604eac7
Cristian Alejandro Forero <cristian-forero@upc.edu.co>
2023-11-27 19:16:57 -0500

    Se añade el codigo final para dados circucales...

--- RollDice/ContentView.swift
+++ RollDice/ContentView.swift
@@ -8,14 +8,77 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let leftCircle = [1,2,3,4,5]
+    let rightCircle = [1,2,3,4,5]
+    
+    @State private var leftEmpty = 0
+    @State private var rightEmpty  = 0
+    
+    @State private var leftValue = 1
+    @State private var rightValue = 2 
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            
+            
+            VStack{
+                Text("Random Dice")
+                Spacer()
+                
+                VStack{
+                    HStack{
+                        Circle().frame(width: 150).overlay{
+                            Text("\(rightValue)")
+                                .foregroundStyle(.white)
+                            
+                        }
+                        Circle().frame(width: 150).overlay{
+                            Text("\(leftValue)")
+                                .foregroundStyle(.white)
+                            
+                        }
+                    }.padding(.horizontal,30)
+                        .padding(.bottom,40)
+                        .font(.system(size: 30,design: .rounded).bold())
+        
+                    Button(action: {
+                        let randomElementleft = Int.random(in: 0...4)
+                        let randomElementRight = Int.random(in: 0...4)
+                        
+                        leftEmpty = leftCircle[randomElementleft]
+                        rightEmpty = rightCircle[randomElementRight]
+                        
+                        withAnimation {
+                            
+                             leftValue = leftEmpty
+                             rightValue = rightEmpty
+                            
+                        }
+                        
+                        
+
+                        
+                        if leftEmpty == rightEmpty{
+                            print("They are the same")
+                            
+                        }else {
+                            print("Are diferent")
+                            print(leftEmpty)
+                            print(rightEmpty)
+                        }
+                        
+                        
+                        
+                    }, label: {
+                        Text("RollDice")
+                    })
+                }
+            }
+            
+            
         }
-        .padding()
     }
 }
 
